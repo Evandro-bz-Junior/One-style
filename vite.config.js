50f808c2706cece49a18b54b0cbc85ea1b63e34f
Evandro-bz-junior <evandrobzjunior@gmail.com>
2024-11-21 23:24:19 -0300

    imgs update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@imgs': '/src/imgs', // A partir de agora você pode usar @imgs como a raiz das imagens
+    },
+  },
 })
