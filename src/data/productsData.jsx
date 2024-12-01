faa026091660804f338c50acab6fdcbc4a100c06
Evandro-bz-junior <evandrobzjunior@gmail.com>
2024-11-30 23:28:50 -0300

    all measurements add

--- src/data/productsData.jsx
+++ src/data/productsData.jsx
@@ -16,6 +16,11 @@ const productsData = [
             getImagePath('Vestido Morgana', 'Vestido Morgana3.jpeg'),
         ],
         description: "Vestido em 100% viscose , blusa forrada no mesmo tecido. Saia possui forro. Fechamento em zíper.",
+        measurements: {
+            busto: { P: "-", M: "-", G: "104cm" },
+            cintura: { P: "-", M: "-", G: "82cm" },
+            comprimento: { P: "-", M: "-", G: "90cm" },
+        },
         isRelease: false
     },
     {
@@ -28,8 +33,14 @@ const productsData = [
             getImagePath('Conjunto Rayele', 'Conjunto Rayele1.jpeg'),
             getImagePath('Conjunto Rayele', 'Conjunto Rayele2.jpeg')
         ],
-        description: `Conjunto em crepe encorpada, trazendo sofisticação e conforto. 
-                        Short possui forro e bolsos laterais tipo faca, cropped possui meio forro e alças ajustáveis por amarração ..`,
+        description: `Conjunto em crepe encorpada, trazendo sofisticação e conforto. Short possui forro e bolsos laterais tipo faca, cropped possui meio forro e alças ajustáveis por amarração.`,
+        measurements: {
+            busto: { P: "86cm", M: "94cm", G: "103cm" },
+            "comprimento cropped": { P: "42cm", M: "45cm", G: "47cm" },
+            cintura: { P: "68 a 72cm", M: "74 a 80 cm", G: "80 a 92cm" },
+            "comprimento short": { P: "45cm", M: "46cm", G: "46cm" },
+            quadril: { P: "100cm", M: "110cm", G: "120cm" },
+        },
         isRelease: false
     },
     {
@@ -44,6 +55,11 @@ const productsData = [
             getImagePath('Vestido Larissa', 'Vestido Larissa3.jpeg'),
         ],
         description: `Forrado com crepe, com alça de amarração , fechamento em zíper.`,
+        measurements: {
+            busto: { P: "84cm", M: "92cm", G: "102cm" },
+            cintura: { P: "70cm", M: "76cm", G: "82cm" },
+            comprimento: { P: "110cm", M: "110cm", G: "110cm" },
+        },
         isRelease: false
     },
     {
@@ -59,6 +75,12 @@ const productsData = [
             getImagePath('Vestido Irani', 'Vestido Irani4.jpeg'),
         ],
         description: `Vestido longo em musseline toque de seda, forrado com crepe, com elástico no ombro  , fechamento em zíper. `,
+        measurements: {
+            busto: { P: "88cm", M: "92cm", G: "102cm" },
+            "comprimento manga": { P: "12cm", M: "15cm", G: "15cm" },
+            cintura: { P: "70cm", M: "76cm", G: "82cm" },
+            "comprimento": { P: "140cm", M: "145cm", G: "145cm" },
+        },
         isRelease: false
     }, {
         id: crypto.randomUUID(),
@@ -72,8 +94,13 @@ const productsData = [
             getImagePath('Conjunto Leilany', 'Conjunto Leilany3.jpeg'),
             getImagePath('Conjunto Leilany', 'Conjunto Leilany4.jpeg'),
         ],
-        description: `Conjunto em crepe, calça pantalona forrado, com fechamento de zíper na lateral,  bolsos faca. Semi cropped com frente forrada no mesmo tecido manga curta.
-`,
+        description: `Conjunto em crepe, calça pantalona forrado, com fechamento de zíper na lateral,  bolsos faca. Semi cropped com frente forrada no mesmo tecido manga curta.`,
+        measurements: {
+            busto: { P: "88cm", M: "96cm", G: "104cm" },
+            "comprimento blusa": { P: "50cm", M: "51cm", G: "53cm" },
+            cintura: { P: "68cm", M: "76cm", G: "82cm" },
+            "comprimento calça": { P: "109cm", M: "111cm", G: "114cm" },
+        },
         isRelease: false
     }, {
         id: crypto.randomUUID(),
@@ -89,6 +116,14 @@ const productsData = [
             getImagePath('Conjunto Monick', 'Conjunto Monick5.jpeg'),
         ],
         description: `Peças  produzids em “tweed”, um tecido estruturado com uma textura característica. Conjunto composto por croppeds  forrado com tricoline. A saia em corte A, possui cintura alta, cós estruturado, fechamento lateral por zíper e forro.`,
+        measurements: {
+            busto: { P: "-", M: "92cm", G: "102cm" },
+            "cintura cropped": { P: "-", M: "78cm", G: "84cm" },
+            "comprimento cropped": { P: "-", M: "42cm", G: "43cm" },
+            quadril: { P: "98cm", M: "104cm", G: "110cm" },
+            "cintura saia": { P: "70cm", M: "76cm", G: "82cm" },
+            "comprimento saia": { P: "43cm", M: "44cm", G: "45cm" },
+        },
         isRelease: false
     }, {
         id: crypto.randomUUID(),
@@ -103,9 +138,15 @@ const productsData = [
             getImagePath('Vestido Paola', 'Vestido Paola4.jpeg'),
         ],
         description: `Vestido em crepe texturizado. Forrado. `,
+        measurements: {
+            busto: { P: "88cm", M: "94cm", G: "104cm" },
+            quadril: { P: "100cm", M: "110cm", G: "120cm" },
+            "comprimento manga": { P: "15cm", M: "15cm", G: "15cm" },
+            comprimento: { P: "81cm", M: "85cm", G: "88cm" },
+        },
         isRelease: false
     },
-    {
+    /* {
         id: crypto.randomUUID(),
         title: "Conjunto Sandra",
         type: 'Conjunto',
@@ -117,8 +158,13 @@ const productsData = [
         ],
         description: `Blusa em linho com viscose, Manga curta, decote V.\n
 Calça em linho com viscose, com fechamento lateral por zíper, possui passantes e forro.`,
+        measurements: {
+            busto: { P: "88cm", M: "92cm", G: "-" },
+            cintura: { P: "70cm", M: "76cm", G: "-" },
+            comprimento: { P: "113cm", M: "114cm", G: "-" },
+        },
         isRelease: false
-    },
+    }, */
     {
         id: crypto.randomUUID(),
         title: "Vestido Kelia",
@@ -132,6 +178,12 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
             getImagePath('Vestido Kelia', 'Vestido Kelia4.jpeg'),
         ],
         description: `Vestido em linho misto leve, blusa forrada no mesmo tecido, saia forrada em helanquinha, possui fenda frontal na saia . Fechamento por zíper nas costas`,
+        measurements: {
+            busto: { P: "-", M: "-", G: "102cm" },
+            cintura: { P: "-", M: "-", G: "82cm" },
+            comprimento: { P: "-", M: "-", G: "110cm" },
+            quadril: { P: "-", M: "-", G: "110cm" },
+        },
         isRelease: false
     },
     {
@@ -145,6 +197,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
             getImagePath('Conjunto Ana', 'Conjunto Ana2.jpeg'),
         ],
         description: `Nossas Peças são linho misto com viscose,, short possui fechamento em zíper nas costas, passante para cinto e forro em poliéster. Blusa estilo básica. `,
+        measurements: {
+            busto: { P: "-", M: "94cm", G: "-" },
+            "comprimento blusa": { P: "-", M: "63cm", G: "-" },
+            "comprimento manga": { P: "-", M: "21cm", G: "-" },
+            "comprimento short": { P: "-", M: "41cm", G: "-" },
+            cintura: { P: "-", M: "76cm", G: "-" },
+            quadril: { P: "-", M: "98cm", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -160,6 +220,11 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
             getImagePath('Camisa Itaguaci', 'Camisa Itaguaci4.jpeg'),
         ],
         description: `Camisa confeccionada em ticoline 100% algodão. `,
+        measurements: {
+            busto: { P: "112cm", M: "-", G: "-" },
+            comprimento: { P: "60cm", M: "-", G: "-" },
+            "comprimento manga": { P: "60cm", M: "-", G: "-" },
+        },
         isRelease: true
     },
     {
@@ -174,6 +239,12 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
             getImagePath('Novo Nathalia', 'Novo Nathalia3.jpeg'),
         ],
         description: `Vestido crepe , possui forro, mangas 3/4, as mangas possuem elástico no ombro , vestido possui forro e fechamento em zíper nas costas. `,
+        measurements: {
+            busto: { P: "88cm", M: "102cm", G: "-" },
+            cintura: { P: "70cm", M: "82cm", G: "-" },
+            comprimento: { P: "90cm", M: "92cm", G: "-" },
+            "comprimento manga": { P: "30cm", M: "30cm", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -184,9 +255,15 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Vestido Emily', 'Vestido Emily1.jpeg'),
-            getImagePath('Vestido Emily', 'Vestido Emily2.jpeg'), 
+            getImagePath('Vestido Emily', 'Vestido Emily2.jpeg'),
         ],
         description: `Vestido em linho misto, mangas levemente bufantes, as mangas possuem elástico no punho , vestido possui forro e fechamento em zíper nas costas.`,
+        measurements: {
+            busto: { P: "88cm", M: "-", G: "-" },
+            cintura: { P: "70cm", M: "-", G: "-" },
+            comprimento: { P: "85cm", M: "-", G: "-" },
+            "comprimento manga": { P: "30cm", M: "-", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -201,6 +278,10 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
             getImagePath('Blusa Rayele', 'Blusa Rayele3.jpeg'),
         ],
         description: `Blusa  em crepe encorpada estilo básica. `,
+        measurements: {
+            busto: { P: "86cm", M: "94cm", G: "104cm" },
+            comprimento: { P: "60cm", M: "61cm", G: "62cm" },
+        },
         isRelease: false
     },
     {
@@ -211,9 +292,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Cropped em Alfaiataria', 'Cropped em Alfaiataria1.jpeg'),
-            getImagePath('Cropped em Alfaiataria', 'Cropped em Alfaiataria2.jpeg'), 
+            getImagePath('Cropped em Alfaiataria', 'Cropped em Alfaiataria2.jpeg'),
         ],
         description: `Cropped de um ombro M em alfaiataria forrado em helanquinha. `,
+        measurements: {
+            busto: { P: "-", M: "90cm", G: "-" },
+            comprimento: { P: "-", M: "45cm", G: "-" },
+            "comprimento manga": { P: "-", M: "31cm", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -224,10 +310,15 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Vestido Carla', 'Vestido Carla1.jpeg'),
-            getImagePath('Vestido Carla', 'Vestido Carla2.jpeg'), 
-            getImagePath('Vestido Carla', 'Vestido Carla3.jpeg'), 
+            getImagePath('Vestido Carla', 'Vestido Carla2.jpeg'),
+            getImagePath('Vestido Carla', 'Vestido Carla3.jpeg'),
         ],
         description: `Vestido mídi em crepe leve, forrado, com zíper nas costas.`,
+        measurements: {
+            busto: { P: "-", M: "92cm", G: "-" },
+            cintura: { P: "-", M: "74cm", G: "-" },
+            comprimento: { P: "-", M: "110cm", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -238,9 +329,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Blusa Fabíola', 'Blusa Fabíola1.jpeg'),
-            getImagePath('Blusa Fabíola', 'Blusa Fabíola2.jpeg'), 
+            getImagePath('Blusa Fabíola', 'Blusa Fabíola2.jpeg'),
         ],
         description: `Blusa em crepe leve com meio forro na frente, manga 3/4 e botões frontais.`,
+        measurements: {
+            busto: { P: "-", M: "-", G: "102cm" },
+            manga: { P: "-", M: "-", G: "39cm" },
+            comprimento: { P: "-", M: "-", G: "60cm," },
+        },
         isRelease: false
     },
     {
@@ -251,10 +347,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Semi cropped Helem', 'Semi cropped Helem1.jpeg'),
-            getImagePath('Semi cropped Helem', 'Semi cropped Helem2.jpeg'), 
-            getImagePath('Semi cropped Helem', 'Semi cropped Helem3.jpeg'), 
+            getImagePath('Semi cropped Helem', 'Semi cropped Helem2.jpeg'),
+            getImagePath('Semi cropped Helem', 'Semi cropped Helem3.jpeg'),
         ],
         description: `Semi cropped em tricoline 100% algodão.`,
+        measurements: {
+            busto: { P: "88cm", M: "94cm", G: "102cm" },
+            comprimento: { P: "47cm", M: "48cm", G: "50cm" },
+        },
         isRelease: false
     },
     {
@@ -265,11 +365,16 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Vestido Melissa', 'Vestido Melissa1.jpeg'),
-            getImagePath('Vestido Melissa', 'Vestido Melissa2.jpeg'), 
-            getImagePath('Vestido Melissa', 'Vestido Melissa3.jpeg'), 
+            getImagePath('Vestido Melissa', 'Vestido Melissa2.jpeg'),
+            getImagePath('Vestido Melissa', 'Vestido Melissa3.jpeg'),
         ],
         description: `Vestido em viscolinho 100% viscose, blusa forrada com mesmo tecido, saia possui forro em poliéster.\n 
         Zíper nas costas . Detalhe trancado no decote.`,
+        measurements: {
+            busto: { P: "-", M: "-", G: "102cm" },
+            cintura: { P: "-", M: "-", G: "82cm" },
+            comprimento: { P: "-", M: "-", G: "102cm" },
+        },
         isRelease: false
     },
     {
@@ -280,10 +385,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Colete paula', 'Colete paula1.jpeg'),
-            getImagePath('Colete paula', 'Colete paula2.jpeg'), 
-            getImagePath('Colete paula', 'Colete paula3.jpeg'), 
+            getImagePath('Colete paula', 'Colete paula2.jpeg'),
+            getImagePath('Colete paula', 'Colete paula3.jpeg'),
         ],
         description: `Colete estruturado, todo forrado no mesmo tecido.`,
+        measurements: {
+            busto: { P: "-", M: "92cm", G: "-" },
+            comprimento: { P: "-", M: "44cm", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -294,9 +403,13 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Cropped Rozeane', 'Cropped Rozeane1.jpeg'),
-            getImagePath('Cropped Rozeane', 'Cropped Rozeane2.jpeg'),  
+            getImagePath('Cropped Rozeane', 'Cropped Rozeane2.jpeg'),
         ],
         description: `Peça produzida em “tweed”, um tecido estruturado com uma textura blusa forrada com tricoline.`,
+        measurements: {
+            busto: { P: "-", M: "-", G: "104cm" },
+            comprimento: { P: "-", M: "-", G: "45cm" },
+        },
         isRelease: false
     },
     {
@@ -307,9 +420,14 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Short Helena', 'Short Helena1.jpeg'),
-            getImagePath('Short Helena', 'Short Helena2.jpeg'),  
+            getImagePath('Short Helena', 'Short Helena2.jpeg'),
         ],
         description: `Short em crepe, short forrado, com fechamento de zíper na lateral.`,
+        measurements: {
+            quadril: { P: "93cm", M: "-", G: "-" },
+            cintura: { P: "68cm", M: "-", G: "-" },
+            comprimento: { P: "40cm", M: "-", G: "-" },
+        },
         isRelease: false
     },
     {
@@ -320,16 +438,16 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Conjunto Sabrina', 'Conjunto Sabrina1.jpeg'),
-            getImagePath('Conjunto Sabrina', 'Conjunto Sabrina2.jpeg'),  
-            getImagePath('Conjunto Sabrina', 'Conjunto Sabrina3.jpeg'),  
+            getImagePath('Conjunto Sabrina', 'Conjunto Sabrina2.jpeg'),
+            getImagePath('Conjunto Sabrina', 'Conjunto Sabrina3.jpeg'),
         ],
         description: `Conjunto em crepe leve,cropped e saia forrados com helanquinha. Possui fechamento em zíper.`,
         measurements: {
-            busto: { P: "-", M: "92cm", G: "-"  },
-            cintura: { P: "-", M: "76cm", G: "82cm"  },
-            "comprimento cropped": { P: "-", M: "38cm", G: "-"  },
+            busto: { P: "-", M: "92cm", G: "-" },
+            cintura: { P: "-", M: "76cm", G: "82cm" },
+            "comprimento cropped": { P: "-", M: "38cm", G: "-" },
             "comprimento saia": { P: "-", M: "42cm", G: "43cm" },
-            quadril: { P: "-", M: "106cm", G: "110cm"  },          
+            quadril: { P: "-", M: "106cm", G: "110cm" },
         },
         isRelease: false
     },
@@ -341,16 +459,16 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Vestido Vitória', 'Vestido Vitória1.jpeg'),
-            getImagePath('Vestido Vitória', 'Vestido Vitória2.jpeg'), 
-            getImagePath('Vestido Vitória', 'Vestido Vitória3.jpeg'), 
-            getImagePath('Vestido Vitória', 'Vestido Vitória4.jpeg'), 
-            getImagePath('Vestido Vitória', 'Vestido Vitória5.jpeg'), 
+            getImagePath('Vestido Vitória', 'Vestido Vitória2.jpeg'),
+            getImagePath('Vestido Vitória', 'Vestido Vitória3.jpeg'),
+            getImagePath('Vestido Vitória', 'Vestido Vitória4.jpeg'),
+            getImagePath('Vestido Vitória', 'Vestido Vitória5.jpeg'),
         ],
         description: `Vestido mídi em crepe, blusa forrada na o mesmo tecido, saia corte godê. Manga estilo baloné, possui zíper nas costas.`,
         measurements: {
-            busto: { P: "88cm", M: "92cm", G: "-"  },
-            cintura: { P: "70cm", M: "76cm", G: "-"  },
-            comprimento: { P: "113cm", M: "114cm", G: "-"  },
+            busto: { P: "88cm", M: "92cm", G: "-" },
+            cintura: { P: "70cm", M: "76cm", G: "-" },
+            comprimento: { P: "113cm", M: "114cm", G: "-" },
         },
         isRelease: true
     },
@@ -362,11 +480,11 @@ Calça em linho com viscose, com fechamento lateral por zíper, possui passantes
         payment: "4x sem juros",
         image: [
             getImagePath('Vestido Marcleia', 'Vestido Marcleia1.jpeg'),
-            getImagePath('Vestido Marcleia', 'Vestido Marcleia2.jpeg'), 
-            getImagePath('Vestido Marcleia', 'Vestido Marcleia3.jpeg'), 
-            getImagePath('Vestido Marcleia', 'Vestido Marcleia4.jpeg'), 
-            getImagePath('Vestido Marcleia', 'Vestido Marcleia5.jpeg'), 
-            getImagePath('Vestido Marcleia', 'Vestido Marcleia6.jpeg'), 
+            getImagePath('Vestido Marcleia', 'Vestido Marcleia2.jpeg'),
+            getImagePath('Vestido Marcleia', 'Vestido Marcleia3.jpeg'),
+            getImagePath('Vestido Marcleia', 'Vestido Marcleia4.jpeg'),
+            getImagePath('Vestido Marcleia', 'Vestido Marcleia5.jpeg'),
+            getImagePath('Vestido Marcleia', 'Vestido Marcleia6.jpeg'),
         ],
         description: `Vestido longo em crepe, blusa forrada na o mesmo tecido, saia levemente franzida, forrada, possui zíper nas costas.`,
         measurements: {
